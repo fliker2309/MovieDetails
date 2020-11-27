4678af76fbe73bc42c886b77029613b8323ea7b2
fliker2309 <60140529+fliker2309@users.noreply.github.com>
2020-11-27 22:45:22 +0300

    inflate movie list into main activity_main.xml

--- app/src/main/java/com/example/moviedetails/FragmentMovieList.kt
+++ app/src/main/java/com/example/moviedetails/FragmentMovieList.kt
@@ -1,23 +1,19 @@
 package com.example.moviedetails
 
-import androidx.fragment.app.Fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 
-
-class FragmentMovieDetails : Fragment() {
+class FragmentMovieList : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_movies_details, container, false)
+        return inflater.inflate(R.layout.fragment_movie_list, container, false)
 
-
-        return view
     }
+
 }
--- app/src/main/java/com/example/moviedetails/MainActivity.kt
+++ app/src/main/java/com/example/moviedetails/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,18 +12,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        supportFragmentManager.beginTransaction()
+            .add(R.id.main_container, FragmentMovieList())
+            .commit()
+
 
 
 
     }
 
 
-    private fun moveToNextScreen(){
 
-        val intent = Intent(this,Moviedetails::class.java)
-
-        startActivity(intent)
-    }
 
 
 
