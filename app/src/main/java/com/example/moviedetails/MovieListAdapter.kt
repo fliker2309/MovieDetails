0e0f88a3befb986512a74ecbaa83e2235c55b46f
fliker2309 <60140529+fliker2309@users.noreply.github.com>
2020-12-03 22:32:09 +0300

    working with MovieListAdapter.kt

--- app/src/main/java/com/example/moviedetails/MovieListAdapter.kt
+++ app/src/main/java/com/example/moviedetails/MovieListAdapter.kt
@@ -1,11 +1,23 @@
 package com.example.moviedetails
 
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+
 import android.view.ViewGroup
+
 import androidx.recyclerview.widget.RecyclerView
 
-class MovieListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+class MovieListAdapter(
+    context: Context
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+        return RecyclerView.ViewHolder(inflater.inflate(R.layout.view_holder_movie, parent, false))
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
@@ -15,4 +27,8 @@ class MovieListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun getItemCount(): Int {
         TODO("Not yet implemented")
     }
+}
+
+class MovieListViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
 }
