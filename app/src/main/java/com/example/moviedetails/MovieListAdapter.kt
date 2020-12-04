d9131922ffd7903dd9af473411515e2112eec5a8
fliker2309 <60140529+fliker2309@users.noreply.github.com>
2020-12-04 20:33:30 +0300

    creating movie_list_header.xml

--- app/src/main/java/com/example/moviedetails/MovieListAdapter.kt
+++ app/src/main/java/com/example/moviedetails/MovieListAdapter.kt
@@ -9,11 +9,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 
-
 class MovieListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-
-
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return MovieListViewHolder(
             LayoutInflater.from(parent.context).inflate(R.layout.view_holder_movie, parent, false)
@@ -24,18 +20,30 @@ class MovieListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         TODO("Not yet implemented")
     }
 
+    override fun getItemViewType(position: Int): Int {
+        return when (position) {
+            0 -> Companion.TYPE_HEADER
+            else -> Companion.TYPE_MOVIES
+        }
+    }
+
     override fun getItemCount(): Int {
         return 1
+    }
+
+    companion object {
+        const val TYPE_HEADER = 0
+        const val TYPE_MOVIES = 1
     }
 }
 
 class MovieListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-   companion object{
-       fun from (parent: ViewGroup):MovieListViewHolder{
-       val inflater = LayoutInflater.from(parent.context)
-       val view = inflater.inflate(R.layout.view_holder_movie, parent, false)
-return MovieListViewHolder(view)
-       }
-   }
+    companion object {
+        fun from(parent: ViewGroup): MovieListViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val view = inflater.inflate(R.layout.view_holder_movie, parent, false)
+            return MovieListViewHolder(view)
+        }
+    }
 
 }
