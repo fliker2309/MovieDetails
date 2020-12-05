1247e75672c377516da9bdb69dd1a548073fa492
fliker2309 <60140529+fliker2309@users.noreply.github.com>
2020-12-06 01:15:23 +0300

    fun bind added

--- app/src/main/java/com/example/moviedetails/UI/MovieList/Adapter/MovieDiffUtilCallback.kt
+++ app/src/main/java/com/example/moviedetails/UI/MovieList/Adapter/MovieDiffUtilCallback.kt
@@ -1,12 +1,13 @@
 package com.example.moviedetails
 
 import androidx.recyclerview.widget.DiffUtil
+import com.example.moviedetails.data.Movie
 
-class MovieDiffUtilCallback : DiffUtil.ItemCallback<DataItem>() {
+class MovieDiffUtilCallback : DiffUtil.ItemCallback<Movie>() {
 
-    override fun areItemsTheSame(oldItem: DataItem, newItem: DataItem): Boolean =
+    override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean =
         oldItem.id == newItem.id
 
-    override fun areContentsTheSame(oldItem: DataItem, newItem: DataItem): Boolean =
+    override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean =
         oldItem == newItem
 }
--- app/src/main/java/com/example/moviedetails/UI/MovieList/FragmentMovieList.kt
+++ app/src/main/java/com/example/moviedetails/UI/MovieList/FragmentMovieList.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,7 +15,7 @@ import com.example.moviedetails.data.Movie
 class MovieListFragment : Fragment() {
 
 
-    private var movieListRecycler: RecyclerView? = null
+    private lateinit var movieListRecycler: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,16 +23,23 @@ class MovieListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val movieList = DataContainer.getAllMovies()
-        val numberOfColums = 2
         val movieListAdapter = MovieListAdapter(
-            cardListener = onMovieLabelClick()
+            movies = movieList,
+            cardListener = {
+                //обрабатывает переход на новый экран
+                Toast.makeText(context, "Worked!", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+
         )
-        val gridLayoutManager = GridLayoutManager(context,2)
+        val gridLayoutManager = GridLayoutManager(context, 2)
 
         val view = inflater.inflate(R.layout.fragment_movie_list, container, false)
+        movieListRecycler = view.findViewById(R.id.movie_list_recycler_view)
+        movieListRecycler.layoutManager = gridLayoutManager
+        movieListRecycler.adapter = movieListAdapter
 
-
-        }
 
         return view
     }
@@ -57,7 +65,7 @@ class MovieListFragment : Fragment() {
         const val TAG = "moviesListFragment"
     }
 
-   private fun onMovieLabelClick(){
+    private fun onMovieLabelClick() {
 
-   }
+    }
 }
