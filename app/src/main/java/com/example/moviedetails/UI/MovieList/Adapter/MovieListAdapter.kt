f37e399ee8f8885908dc922aae1248ca6c696df6
fliker2309 <60140529+fliker2309@users.noreply.github.com>
2020-12-05 23:16:05 +0300

    worked version

--- app/src/main/java/com/example/moviedetails/UI/MovieList/Adapter/MovieListAdapter.kt
+++ app/src/main/java/com/example/moviedetails/UI/MovieList/Adapter/MovieListAdapter.kt
@@ -1,67 +1,49 @@
 package com.example.moviedetails
 
-import android.content.Context
+
 import android.view.LayoutInflater
-import android.view.LayoutInflater.*
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import com.example.moviedetails.data.Movie
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
-
 
 class MovieListAdapter(
-    context: Context,
     private val movies: List<Movie>,
-    private val cardListener: (Long) -> Unit
-) : RecyclerView.Adapter<ViewHolder>() {
-    private val inflater: LayoutInflater = from(context)
+    private val cardListener: (Long) -> Unit,
+) : RecyclerView.Adapter<MovieListViewHolder>() {
 
-
-    fun getMovieCount(): Int = movies.size
-
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return MovieListViewHolder(inflater.inflate(R.layout.view_holder_movie, parent, false))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieListViewHolder {
+        val inflater: LayoutInflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.view_holder_movie, parent, false)
+        return MovieListViewHolder(view, cardListener)
     }
 
-    override fun onBindViewHolder(movie: Movie, cardListener: (Long) -> Unit) {
-
+    override fun onBindViewHolder(holder: MovieListViewHolder, position: Int) {
+        holder.bind(movies[position])
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = movies.size
 }
 
+class MovieListViewHolder(view: View, private val cardListener: (Long) -> Unit) :
+    RecyclerView.ViewHolder(view) {
+    private val labelImage: ImageView = view.findViewById(R.id.label_avengers)
+    private val movieName: TextView = view.findViewById(R.id.label_avengers_movie)
+//
 
-class MovieListViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-    // creating variables
-
-
-    fun bind(movie: Movie, cardListener: (Long) -> Unit) {
-
-    }
-
-
-    //  companion object {
-    //    fun from(parent: ViewGroup): MovieListViewHolder {
-    //      val inflater = from(parent.context)
-    //    val view = inflater.inflate(R.layout.view_holder_movie, parent, false)
-    //  return MovieListViewHolder(view)
-    // }
-    //  }
-}
-
-sealed class DataItem {
-    data class MovieItem(val movie: Movie) : DataItem() {
-        override val id: Long
-            get() = movie.id
+    fun bind(movie: Movie) {
+        labelImage.setImageResource(movie.label_image)
+        movieName.text = movie.movie_name
+        itemView.setOnClickListener {
+            cardListener.invoke(movie.id)
+        }
 
     }
 
 
-    abstract val id: Long
 }
+
 
 
