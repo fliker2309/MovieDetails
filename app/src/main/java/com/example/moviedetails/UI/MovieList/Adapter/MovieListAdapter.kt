cc07475405b4598c6abf53ee961d46a0852e1caa
fliker2309 <60140529+fliker2309@users.noreply.github.com>
2020-12-06 01:22:56 +0300

    delete moviedetails activity

--- app/src/main/java/com/example/moviedetails/UI/MovieList/Adapter/MovieListAdapter.kt
+++ app/src/main/java/com/example/moviedetails/UI/MovieList/Adapter/MovieListAdapter.kt
@@ -31,11 +31,27 @@ class MovieListViewHolder(view: View, private val cardListener: (Long) -> Unit) 
     RecyclerView.ViewHolder(view) {
     private val labelImage: ImageView = view.findViewById(R.id.label_avengers)
     private val movieName: TextView = view.findViewById(R.id.label_avengers_movie)
+    private val pegiInfo: TextView = view.findViewById(R.id.pegi_info)
+    private val genre: TextView = view.findViewById(R.id.text_genre)
+    private val reviews: TextView = view.findViewById(R.id.reviews_quantity)
+    private val duration: TextView = view.findViewById((R.id.avengers_movie_duration))
+    private val likeIcon: ImageView = view.findViewById(R.id.ic_like)
+
 //
 
     fun bind(movie: Movie) {
         labelImage.setImageResource(movie.label_image)
         movieName.text = movie.movie_name
+        pegiInfo.text = movie.pegi_info
+        genre.text = movie.text_genre
+        reviews.text = movie.reviews_quantity
+        duration.text = movie.duration
+        likeIcon.setImageResource(movie.ic_like)
+
+
+
+
+
         itemView.setOnClickListener {
             cardListener.invoke(movie.id)
         }
