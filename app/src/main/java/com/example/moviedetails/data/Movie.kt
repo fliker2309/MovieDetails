d8ee8e7a658e5f630e2ded693664236902ecc54d
fliker2309 <60140529+fliker2309@users.noreply.github.com>
2020-12-15 21:19:02 +0300

    add coroutines dependencies, add data and assets

--- app/src/main/java/com/example/moviedetails/data/Movie.kt
+++ app/src/main/java/com/example/moviedetails/data/Movie.kt
@@ -1,35 +1,16 @@
 package com.example.moviedetails.data
 
-import androidx.annotation.DrawableRes
 
 data class Movie(
-    val id: Long,
-    @DrawableRes
-    val label_image: Int,
-    @DrawableRes
-    val movie_background_image: Int,
-    @DrawableRes
-    val movie_background_gradient: Int,
-    @DrawableRes
-    val label_background_linear: Int,
-    val pegi_info: String,
-    @DrawableRes
-    val ic_like: Int,
-    val text_genre: String,
-    @DrawableRes
-    val first_star_icon: Int,
-    @DrawableRes
-    val second_star_icon: Int,
-    @DrawableRes
-    val third_star_icon: Int,
-    @DrawableRes
-    val fourth_star_icon: Int,
-    @DrawableRes
-    val fifth_star_icon: Int,
-    val rating_bar: Int,
-    val story_line: String,
-    val reviews_quantity: String,
-    val cast: List<Actor>,
-    val duration: String,
-    val movie_name: String
+    val id: Int,
+    val title: String,
+    val overview: String,
+    val poster: String,
+    val backdrop: String,
+    val ratings: Float,
+    val numberOfRatings: Int,
+    val minimumAge: Int,
+    val runtime: Int,
+    val genres: List<Genre>,
+    val actors: List<Actor>
 )
