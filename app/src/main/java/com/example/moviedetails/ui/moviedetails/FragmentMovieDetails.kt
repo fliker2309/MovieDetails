3af6f8b6797a14f4255691a4a7df08693732f634
fliker2309 <60140529+fliker2309@users.noreply.github.com>
2020-12-20 16:04:52 +0300

    fix FragmentMovieDetails.kt

--- app/src/main/java/com/example/moviedetails/ui/moviedetails/FragmentMovieDetails.kt
+++ app/src/main/java/com/example/moviedetails/ui/moviedetails/FragmentMovieDetails.kt
@@ -14,6 +14,8 @@ import com.bumptech.glide.Glide
 import kotlinx.coroutines.launch
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.moviedetails.data.Movie
+import com.example.moviedetails.ui.MainActivity
 import com.example.moviedetails.ui.R
 import com.example.moviedetails.ui.moviedetails.adapter.MovieDetailsAdapter
 
@@ -23,12 +25,14 @@ class MovieDetailsFragment : Fragment() {
 
     private var movieId: Int? = null
     private lateinit var actorListRecycler: RecyclerView
+    private lateinit var movie: Movie
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             movieId = it.getInt(MOVIE_ID_KEY)
         }
+        movie = MainActivity.movies.single { it.id == movieId }
     }
 
     override fun onCreateView(
@@ -36,28 +40,43 @@ class MovieDetailsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val view = inflater.inflate(R.layout.fragment_movies_details, container, false)
+
+        return inflater.inflate(R.layout.fragment_movies_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         val backgroundImage: ImageView = view.findViewById(R.id.background)
         val minimumAge: TextView = view.findViewById(R.id.minimum_age)
         val movieTitle: TextView = view.findViewById(R.id.movie_title)
         val genre: TextView = view.findViewById(R.id.genre)
         val rating: RatingBar = view.findViewById(R.id.ratingBar)
-        val reviews: TextView = view.findViewById(R.id.total_reviews)
+        val totalReviews: TextView = view.findViewById(R.id.total_reviews)
         val storyLine: TextView = view.findViewById(R.id.overview)
 
-      !!  val cast = movie?.cast
-        val movieDetailsAdapter = cast?.let {
-            MovieDetailsAdapter(
-                actors = cast
-            )
+        view.findViewById<Button>(R.id.back_to_main_button)?.setOnClickListener {
+            activity?.onBackPressed()
         }
-        !!movie?.let {
-            backgroundImage.setImageResource(movie.movie_background_image)
-            pegiInfo.text = movie.pegi_info
-            movieName.text = movie.movie_name
-            genre.text = movie.text_genre
-            rating.numStars = movie.rating_bar
-            reviews.text = movie.reviews_quantity
+        val cast = movie.actors
+        val movieDetailsAdapter = MovieDetailsAdapter(actors = cast)
+
+        movie.let {
+            Glide
+                .with(this)
+                .load(movie.backdrop)
+                .centerCrop()
+                .placeholder(R.drawable.ic_image_download)
+                .error(R.drawable.ic_image_download)
+                .into(backgroundImage)
+
+            minimumAge.text = context!!.getString(
+                R.string.pg_rating,
+                movie.minimumAge.toString()
+            )
+            movieTitle.text = movie.title
+            genre.text = movie.genres.toString()
+            rating.rating= convertRating(movie.ratings)
+            totalReviews.text = movie.numberOfRatings.toString()
             storyLine.text = movie.overview
             actorListRecycler = view.findViewById(R.id.actor_list_recycler_view)
             val linearLayoutManager =
@@ -65,10 +84,6 @@ class MovieDetailsFragment : Fragment() {
             actorListRecycler.layoutManager = linearLayoutManager
             actorListRecycler.adapter = movieDetailsAdapter
         }
-        view.findViewById<Button>(R.id.back_to_main_button)?.setOnClickListener {
-            activity?.onBackPressed()
-        }
-        return view
     }
 
     companion object {
@@ -80,4 +95,10 @@ class MovieDetailsFragment : Fragment() {
             }
         }
     }
+
+    private fun convertRating(rating10: Float): Float = rating10 / 2.0f
 }
+
+
+
+
