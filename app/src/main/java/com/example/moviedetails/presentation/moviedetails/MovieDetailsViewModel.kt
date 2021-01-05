61bab03cec34dc5fbb0b36942a672ae8d0816ce3
fliker2309 <60140529+fliker2309@users.noreply.github.com>
2021-01-05 23:59:36 +0300

    add viewmodelfactory

--- app/src/main/java/com/example/moviedetails/presentation/moviedetails/MovieDetailsViewModel.kt
+++ app/src/main/java/com/example/moviedetails/presentation/moviedetails/MovieDetailsViewModel.kt
@@ -1,14 +1,16 @@
 package com.example.moviedetails.presentation.moviedetails
 
 import android.app.Application
+import android.app.VoiceInteractor
 import androidx.lifecycle.*
 import com.example.moviedetails.data.Movie
 import com.example.moviedetails.data.loadMovies
+import com.example.moviedetails.domain.MovieInteractor
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class MovieDetailsViewModel(private val app: Application) : AndroidViewModel(app) {
+class MovieDetailsViewModel(private val interactor: MovieInteractor) : ViewModel() {
 
     private var _movieLiveData: MutableLiveData<Movie> = MutableLiveData<Movie>()
     val movieLiveData: LiveData<Movie>
