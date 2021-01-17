a8f285c1567ba6d38bf1d20b67ab2fa7153234dd
fliker2309 <60140529+fliker2309@users.noreply.github.com>
2021-01-17 17:34:33 +0300

    fix PR mistakes

--- app/src/main/java/com/example/moviedetails/network/ApiKeyInterceptor.kt
+++ app/src/main/java/com/example/moviedetails/network/ApiKeyInterceptor.kt
@@ -3,10 +3,14 @@ package com.example.moviedetails.network
 import okhttp3.Interceptor
 import okhttp3.Response
 
-class ApiKeyInterceptor  : Interceptor{
+const val API_KEY = "63465eb594a4f703d9ae4b0842de07a5"
+const val LANG = "en-US"
+
+class ApiKeyInterceptor : Interceptor {
 
     companion object {
         private const val AUTHORIZATION_HEADER = "Authorization"
+        private const val LANGUAGE_HEADER = "Language"
     }
 
     override fun intercept(chain: Interceptor.Chain): Response {
@@ -16,7 +20,9 @@ class ApiKeyInterceptor  : Interceptor{
         val request = originalRequest.newBuilder()
             .url(originalHttpUrl)
             .addHeader(AUTHORIZATION_HEADER, API_KEY)
+            .addHeader(LANGUAGE_HEADER, LANG)
             .build()
+
         return chain.proceed(request)
     }
 }
