01c5d25e04670799b845568d4f01c363ee9fd993
fliker2309 <60140529+fliker2309@users.noreply.github.com>
2021-01-17 18:08:36 +0300

    fix ApiKeyInterceptor.kt

--- app/src/main/java/com/example/moviedetails/network/TheMovieDbApi.kt
+++ app/src/main/java/com/example/moviedetails/network/TheMovieDbApi.kt
@@ -5,18 +5,18 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface TheMovieDbApi {
-    @GET("configuration?api_key=$API_KEY")
+    @GET("configuration")
     suspend fun getConfiguration(): Configuration
 
-    @GET("movie/now_playing?api_key=$API_KEY&language=$LANG&page=1")
+    @GET("movie/now_playing?")
     suspend fun getMovies(): Movies
 
-    @GET("genre/movie/list?api_key=$API_KEY&language=$LANG")
+    @GET("genre/movie/list")
     suspend fun getGenres(): Genres
 
-    @GET("movie/{id}/credits?api_key=$API_KEY&language=$LANG")
+    @GET("movie/{id}/credits")
     suspend fun getCredits(@Path("id") movieId: Int): Credits
 
-    @GET("movie/{id}?api_key=$API_KEY&language=$LANG")
+    @GET("movie/{id}")
     suspend fun getDetails(@Path("id") movieId: Int): Details
 }
