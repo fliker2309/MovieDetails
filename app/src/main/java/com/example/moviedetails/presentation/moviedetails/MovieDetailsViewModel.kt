eb4ec6efb10ddbe1c670b5ebf91cff32fb6ef997
Evgenii <60140529+fliker2309@users.noreply.github.com>
2021-01-21 20:25:04 +0300

    add sealed classes into MovieListViewModel.kt.kt

--- app/src/main/java/com/example/moviedetails/presentation/moviedetails/MovieDetailsViewModel.kt
+++ app/src/main/java/com/example/moviedetails/presentation/moviedetails/MovieDetailsViewModel.kt
@@ -31,4 +31,5 @@ sealed class Result<out T : Any> {
 
     data class Success<out T : Any>(val data: T) : Result<T>()
     data class Error(val message: String) : Result<Nothing>()
+
 }
--- app/src/main/java/com/example/moviedetails/presentation/movielist/MovieListViewModel.kt
+++ app/src/main/java/com/example/moviedetails/presentation/movielist/MovieListViewModel.kt
@@ -26,3 +26,10 @@ class MovieListViewModel : ViewModel() {
         }
     }
 }
+
+sealed class Result<out T : Any> {
+
+    data class Success<out T : Any>(val data: T) : Result<T>()
+    data class Error(val message: String) : Result<Nothing>()
+
+}
