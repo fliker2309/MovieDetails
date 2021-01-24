358553be5b31224f2c6b12eeabaa10095e7ece0a
fliker2309 <60140529+fliker2309@users.noreply.github.com>
2021-01-24 15:38:34 +0300

    fix MovieEntity.kt

--- app/src/main/java/com/example/moviedetails/data/db/entity/MovieEntity.kt
+++ app/src/main/java/com/example/moviedetails/data/db/entity/MovieEntity.kt
@@ -4,53 +4,43 @@ import androidx.room.ColumnInfo
 import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-
+import com.example.moviedetails.data.Actor
+import com.example.moviedetails.data.model.Genre
 
 @Entity(tableName = "movies")
 data class MovieEntity(
     @PrimaryKey
     @ColumnInfo(name = "id")
-    val id: Long?,
+    val id: Long,
 
     @ColumnInfo(name = "title")
-    val title: String?,
+    val title: String,
 
     @ColumnInfo(name = "pg")
-    val pgRating: String?,
+    val pgRating: String,
 
     @ColumnInfo(name = "poster_path")
-    val posterPath: String? = null,
+    val posterPath: String,
 
     @ColumnInfo(name = "rating")
-    val voteAverage: Long?,
+    val voteAverage: Long,
 
     @ColumnInfo(name = "vote_count")
-    val voteCount: Long?,
+    val voteCount: Long,
 
     @ColumnInfo(name = "backdrop_path")
-    val backdropPath: String? = null,
+    val backdropPath: String,
 
     @ColumnInfo(name = "runtime")
-    val runtime: String?,
+    val runtime: String,
 
     @ColumnInfo(name = "overview")
-    val overview: String?,
+    val overview: String,
 
-//сконвертировать в строку при записи в дао
-    @ColumnInfo(name = "genre")
-    val genre: String?,
+    @ColumnInfo(name = "genres")
+    val genre: List<Genre>,
 
-    @Embedded
-    val actor: Actor?
+    @ColumnInfo(name = "actors")
+    val actor: List<Actor>
 )
 
-data class Actor(
-    @ColumnInfo(name = "actor_id")
-    val actorId: Long?,
-
-    @ColumnInfo(name = "name")
-    val name: String,
-
-    @ColumnInfo(name = "profile_path")
-    val profilePath: String? = null
-)
