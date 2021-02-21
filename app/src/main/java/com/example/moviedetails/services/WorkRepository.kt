b9bc2ed7fae4a37863915689b4ede0106461deb1
Zhenya <evgeniikapchik@gmail.com>
2021-02-21 16:53:54 +0300

    init commit

--- app/src/main/java/com/example/moviedetails/services/WorkRepository.kt
+++ app/src/main/java/com/example/moviedetails/services/WorkRepository.kt
@@ -12,7 +12,7 @@ class WorkRepository {
         .setRequiredNetworkType(NetworkType.CONNECTED)
         .build()
 
-    val constraintsRequest = PeriodicWorkRequest.Builder(SynchronizationWorker::class.java, 1, TimeUnit.HOURS)
+    val constraintsRequest = PeriodicWorkRequest.Builder(SynchronizationWorker::class.java, 8, TimeUnit.HOURS)
         .setConstraints(constraints)
         .build()
 }
